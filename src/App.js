15cd265328eeb99f49b7097f7dd5aa1c778d2d91
Khaoula <99682033+khaoula-khemiri@users.noreply.github.com>
2022-03-14 09:48:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+
+
+function App(){
+
+  const [calc,setCalc] = useState("");
+  const [result,setResult] = useState("");
+  const ops = ['/','*','+','.','-'];
+
+  const updateCalc  = value =>{
+
+    if(ops.includes(value) && calc === '' || 
+    ops.includes(value) && ops.includes(calc.slice(-1)))
+    {return;}
+   setCalc(calc + value);
+   if(!ops.includes(value)){
+     setResult(eval(calc + value).toString());
+    }
+  }
+
+  const calculate = () =>{
+    setCalc(eval(calc).toString());
+  }
+ 
+  const deleteLast = () =>{
+    if(calc == ''){
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      
+      
+      <div className = 'calculator'>
+       <div className = 'display'>
+          {result ? <span>({result})</span> : ''} &nbsp; {calc || "0"}
+       </div>
+
+       <div className = "operators">
+          <button onClick = {() =>updateCalc('/')}  value="/"> / </button>
+          <button onClick = {() =>updateCalc('*')}  value="*"> x </button>
+          <button onClick = {() =>updateCalc('+')}  value="+"> + </button>
+          <button onClick = {() =>updateCalc('-')}  value="-"> ‑ </button> 
+          <button onClick = {deleteLast}  value="Ac"> AC </button>
+       </div>
+
+       <div className="digits">
+         <button onClick = {() =>updateCalc('1')}  value="1"> 1 </button>
+         <button onClick = {() =>updateCalc('2')}  value="2"> 2 </button>
+         <button onClick = {() =>updateCalc('3')}  value="3"> 3 </button>
+         <button onClick = {() =>updateCalc('4')}  value="4"> 4 </button>
+         <button onClick = {() =>updateCalc('5')}  value="5"> 5 </button> 
+         <button onClick = {() =>updateCalc('6')}  value="6"> 6 </button>
+         <button onClick = {() =>updateCalc('7')}  value="7"> 7 </button>
+         <button onClick = {() =>updateCalc('8')}  value="8"> 8 </button>
+         <button onClick = {() =>updateCalc('9')}  value="9"> 9 </button>
+         <button onClick = {() =>updateCalc('0')}  value="0"> 0 </button>  
+         <button onClick = {() =>updateCalc('.')}  value="."> . </button>
+         <button onClick = {calculate}  value="="> = </button>
+
+        </div>
+     </div>
+     
     </div>
   );
 }
